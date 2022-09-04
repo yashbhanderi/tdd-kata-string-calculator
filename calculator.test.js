4b97aa808d0986bfa031d28f96582e0fe2a3920e
Yash Bhanderi <yashbhanderi1818@gmail.com>
2022-09-05 00:44:36 +0530

    3) Unknown amount of numbers string test

--- calculator.test.js
+++ calculator.test.js
@@ -10,3 +10,8 @@ test("Comma, One digit string test", () => {
     expect(sum("2,7")).toBe(9);
     expect(sum("97,98")).toBe(195);
 });
+
+test("Unknown amount of numbers string test", () => {
+    expect(sum("2,7,3,1,6,8,43,2,3")).toBe(75);
+    expect(sum("10,332,23,134,500,1,1,33,444")).toBe(1478);
+});
